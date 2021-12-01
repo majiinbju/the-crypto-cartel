41525cf65cda142e9972051634c183dc1bfd5be0
Glitch (glitch-hello-website) <none>
2021-12-01 20:19:43 +0000

    🐸👞 Checkpoint ./style.css:3198319/4764 ./index.html:3198319/291 ./script.js:3198319/1198

---
+++ script.js
@@ -0,0 +1,56 @@
+/* Function to Smoothen the Scroll */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/* Space Particles in the Background */
+
+var $window = $(window);
+
+$window.load(function() {
+  var dotContainer = $("#dotContainer");
+
+  dotContainer.height($window.height());
+
+  function getParticlesAnimation() {
+    i = 500,
+      dots = [];
+
+    while (--i > -1) {
+      dot = document.createElement('div');
+      dot.className = 'dot';
+      dotContainer.append(dot);
+      tween(dot);
+    }
+  }
+  
+  function tween(element) {
+    console.log("tween")
+    TweenMax.set(element, {
+        x: Math.random() * $(window).width(),
+        y: Math.random() * $(window).height(),
+        z: Math.random() * Math.PI * 100
+      });
+      TweenMax.to(element, Math.random() * 10, {
+        z: 3000,
+        onComplete:tween,
+        
+        onCompleteParams:[element]
+      });
+  }
+  
+  
+  
+  
+  getParticlesAnimation();
+
+}).resize(function() {
+  dotContainer.height($window.height());
+});
