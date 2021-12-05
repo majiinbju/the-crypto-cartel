ffb7bca399cf302cc8611f06bb7eab882676c1db
Glitch (glitch-hello-website) <none>
2021-12-05 07:35:51 +0000

    📉🎂 Checkpoint ./meta-style.css:3198319/161 ./script.js:3198319/132437 ./metavilla.html:3198319/19

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var firstsec = document.getElementById('first-section');
+var secondsec = document.getElementById('about');
+var lastScrollTop = 0;
+window.onscroll = function(){
+   var st = window.pageYOffset || document.documentElement.scrollTop; 
+   if (st > lastScrollTop){
+      secondsec.scrollIntoView({behavior: "smooth"});
+   } else {
+      firstsec.scrollIntoView({behavior: "smooth"});
+   }
+   lastScrollTop = st <= 0 ? 0 : st; 
+} 
+
 
 /* FAQ Function */ 
 
@@ -84,3 +97,5 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
