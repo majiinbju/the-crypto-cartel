2ca5438eb397532ab8f9a65a72f11f6f80425eb4
Glitch (glitch-hello-website) <none>
2021-12-05 04:03:52 +0000

    👂📯 Checkpoint ./index.html:3198319/7892 ./navbar-style.css:3198319/646 ./metavilla.html:3198319/1409 ./script.js:3198319/1866 ./index-style.css:3198319/4

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 
+/* FAQ Function */ 
+
+var shows = document.querySelectorAll(".show");
+var infos = document.querySelectorAll(".info");
+var accordions = document.querySelectorAll(".accordion");
+
+for (let i = 0; i < shows.length; i++) {
+  shows[i].onclick = function() {
+    for (let j = 0; j < shows.length; j++) {
+      if (shows[j] != this) {
+        infos[j].classList.remove("reveal");
+        shows[j].innerHTML = "+";
+        accordions[j].classList.remove("radius");
+      }
+    }
+    accordions[i].classList.toggle("radius");
+    infos[i].classList.toggle("reveal");
+    if (infos[i].className.includes("reveal")) shows[i].innerHTML = "-";
+    else shows[i].innerHTML = "+";
+  };
+}
+
+/* Navbar Expand */
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+
 /* Space Particles in the Background */
 
 var $window = $(window);
@@ -44,35 +76,3 @@ $window.load(function() {
   dotContainer.height($window.height());
 });
 
-
-
-/* FAQ Function */ 
-
-var shows = document.querySelectorAll(".show");
-var infos = document.querySelectorAll(".info");
-var accordions = document.querySelectorAll(".accordion");
-
-for (let i = 0; i < shows.length; i++) {
-  shows[i].onclick = function() {
-    for (let j = 0; j < shows.length; j++) {
-      if (shows[j] != this) {
-        infos[j].classList.remove("reveal");
-        shows[j].innerHTML = "+";
-        accordions[j].classList.remove("radius");
-      }
-    }
-    accordions[i].classList.toggle("radius");
-    infos[i].classList.toggle("reveal");
-    if (infos[i].className.includes("reveal")) shows[i].innerHTML = "-";
-    else shows[i].innerHTML = "+";
-  };
-}
-
-/* Navbar Expand */
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
