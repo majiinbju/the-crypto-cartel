3231ebb458ff14c452ccfad9f6ceee80fa04dcfc
Glitch (glitch-hello-website) <none>
2021-12-05 04:39:01 +0000

    ⛱🍉 Checkpoint ./navbar-style.css:3198319/193 ./index-style.css:3198319/32 ./index.html:3198319/396 ./metavilla.html:3198319/394 ./script.js:3198319/274 ./meta-style.css:3198319/8

--- script.js
+++ script.js
@@ -74,3 +74,13 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
